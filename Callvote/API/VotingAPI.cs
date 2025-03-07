91260bf7e2606bbd56ce38ca88b341144229577c
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2025-03-06 21:36:34 -0300

    Add new .callvote custom syntax
    .callvote custom "(Question with spaces)" option1(optionInfo) option2(optionInfo) ...
    .callvote custom (Question without spaces) option1(optionInfo) option2(optionInfo) ...
    .callvote custom (Question without spaces) option1 option2 ...
    
    Democracy wins again

--- Callvote/API/VotingAPI.cs
+++ Callvote/API/VotingAPI.cs
@@ -135,6 +135,7 @@ namespace Callvote.VoteHandlers
                 }
             }
         }
+
     }
 }
 
--- Callvote/Commands/CustomVotingCommand.cs
+++ Callvote/Commands/CustomVotingCommand.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Callvote.VoteHandlers;
 using CommandSystem;
+using Exiled.API.Extensions;
 using Exiled.API.Features;
 using Exiled.Permissions.Extensions;
+using static Exiled.API.Features.DamageHandlers.DamageHandlerBase;
 
 namespace Callvote.Commands
 {
@@ -19,7 +22,8 @@ namespace Callvote.Commands
         public bool Execute(ArraySegment<string> args, ICommandSender sender, out string response)
         {
             Dictionary<string, string> options = new Dictionary<string, string>();
-
+            List<string> argsStrings = JoinWordsBetweenQuotes(args);
+            List<string> optionDetailsStrings = ExtractAndRemoveParenthesesValues(ref argsStrings);
             Player player = Player.Get(sender);
 
             if (!player.CheckPermission("cv.callvotecustom") || !player.CheckPermission("cv.bypass"))
@@ -28,25 +32,88 @@ namespace Callvote.Commands
                 return false;
             }
 
-            if (args.Count < 2)
+
+            if (argsStrings.Count < 2)
             {
                 response = Plugin.Instance.Translation.LessThanTwoOptions;
                 return false;
             }
 
-            foreach (var option in args.Skip(1))
+            for (int i = 1; i < argsStrings.Count; i++) // args = 3
             {
-                if (options.ContainsKey(option))
+                string optionDetail;
+                if (!optionDetailsStrings.TryGet(i-1, out optionDetail))
                 {
-                    response = Plugin.Instance.Translation.DuplicateCommand;
-                    return false;
+                    optionDetail = argsStrings[i];
                 }
-
-                options.Add(option, option);
+                options.Add(argsStrings[i], optionDetail);
             }
-            VotingAPI.CurrentVoting = new Voting(Plugin.Instance.Translation.AskedCustom.Replace("%Player%", player.Nickname).Replace("%Custom%", args.ElementAt(0)), options, player, null);
+            VotingAPI.CurrentVoting = new Voting(Plugin.Instance.Translation.AskedCustom.Replace("%Player%", player.Nickname).Replace("%Custom%", argsStrings.First()), options, player, null);
             response = VotingAPI.CurrentVoting.Response;
             return true;
+        }
+
+        static List<string> JoinWordsBetweenQuotes(ArraySegment<string> words)
+        {
+            List<string> finalList = new List<string>();
+            bool insideQuotes = false;
+            List<string> wordsBetweenQuotes = new List<string>();
+
+            foreach (string word in words)
+            {
+                if (word.StartsWith("\""))
+                {
+                    insideQuotes = true;
+                    wordsBetweenQuotes.Clear(); // Start new group
+                }
+
+                if (insideQuotes)
+                {
+                    wordsBetweenQuotes.Add(word.Trim('"'));
+
+                    if (word.EndsWith("\"") && word.Length > 1)
+                    {
+                        finalList.Add(string.Join(" ", wordsBetweenQuotes));
+                        insideQuotes = false;
+                    }
+                }
+                else
+                {
+                    finalList.Add(word);
+                }
+            }
+
+            return finalList;
+        }
+
+        static List<string> ExtractAndRemoveParenthesesValues(ref List<string> list)
+        {
+            List<string> parenthesesList = new List<string>();
+            Regex regex = new Regex(@"\(([^)]+)\)");
+
+            List<string> cleanedList = new List<string>();
+
+            foreach (string item in list)
+            {
+                MatchCollection matches = regex.Matches(item);
+                string cleanedItem = item;
+
+                foreach (Match match in matches)
+                {
+                    parenthesesList.Add(match.Groups[1].Value);
+                    cleanedItem = cleanedItem.Replace(match.Value, "").Trim();
+                }
+
+                if (!string.IsNullOrEmpty(cleanedItem))
+                {
+                    cleanedList.Add(cleanedItem);
+                }
+            }
+
+            list.Clear();
+            list.AddRange(cleanedList);
+
+            return parenthesesList;
         }
     }
 }
