1a55668e55588ac15c8a1e43e594dddbf2294b7a
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2025-04-07 22:24:38 -0300

    FIx code typo

--- Callvote/Properties/AssemblyInfo.cs
+++ Callvote/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ namespace Callvote
         internal const string Id = "unbistrackted.Callvote";
         internal const string ConfigPrefix = "Callvote";
         internal const string LangFile = "Callvote";
-        internal const string Version = "6.2.0";
+        internal const string Version = "6.2.1";
     }
 }
