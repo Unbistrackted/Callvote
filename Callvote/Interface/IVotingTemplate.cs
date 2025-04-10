99f295b0cb00adbc18e4fedac849111dd9073126
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2025-04-10 17:03:34 -0300

    Add IVotingTemplate

---
+++ Callvote/Interface/IVotingTemplate.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Features;
+using System.Collections.Generic;
+
+namespace Callvote.Interface
+{
+    public interface IVotingTemplate
+    {
+        Player CallVotePlayer { get; }
+        string Question { get; }
+        string VotingType { get; }
+        CallvoteFunction Callback { get; }
+        Dictionary<string, string> Options { get; }
+    }
+}
