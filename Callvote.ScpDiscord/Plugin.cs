0840844055dc758025d9df4e869619e2211f53f7
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2026-03-17 11:20:39 -0300

    feat: add csproj for scpdiscord

--- Callvote.ScpDiscord/Plugin.cs
+++ Callvote.ScpDiscord/Plugin.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Callvote.ScpDiscord.Configuration;
+using LabApi.Loader.Features.Plugins;
+using System;
 
 namespace Callvote.ScpDiscord
 {
-    internal class Plugin
+    public class Plugin : Plugin<Config>
     {
+        public static Plugin Instance { get; private set; }
+
+        public override string Name => throw new NotImplementedException();
+
+        public override string Description => throw new NotImplementedException();
+
+        public override string Author => throw new NotImplementedException();
+
+        public override Version Version => throw new NotImplementedException();
+
+        public override Version RequiredApiVersion => throw new NotImplementedException();
+
+        public override void Disable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Enable()
+        {
+            Instance = this;
+            throw new NotImplementedException();
+        }
     }
 }
